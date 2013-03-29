2e6cd920c0c1e58d1dfecdd0c9f583c2edb1abfe
Nicolae Namolovan <nna@zenexity.com>
2013-03-29 11:35:58 +0100

    Controller bootstrap

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,50 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+// Reactive Mongo imports
+import reactivemongo.api._
+import reactivemongo.bson._
+import reactivemongo.bson.handlers.DefaultBSONHandlers._
+
+// Reactive Mongo plugin
+import play.modules.reactivemongo._
+import play.modules.reactivemongo.PlayBsonImplicits._
+
+// Play Json imports
+import play.api.libs.json._
+
+import play.api.Play.current
+
+object Application extends Controller with MongoController {
+  val db = ReactiveMongoPlugin.db
+  lazy val collection = db("videos")
+
+  def index = Action { Ok("works") }
+
+  // creates a new Person building a JSON from parameters
+  def addUrl(url: String) = Action {
+    Async {
+      val homePage: Future[play.api.libs.ws.Response] = WS.url("http://mysite.com").get()
+      val json = Json.obj(
+        "url" -> url,
+        "created" -> new java.util.Date().getTime()
+      )
+
+      collection.insert[JsValue]( json ).map( lastError =>
+        Ok("Mongo LastErorr:%s".format(lastError))
+      )
+    }
   }
-  
+
+  // queries for a person by name
+  /*def findByName(name: String) = Action {
+    Async {
+      val qb = QueryBuilder().query(Json.obj( "name" -> name )).sort( "created" -> SortOrder.Descending)
+
+      collection.find[JsValue]( qb ).toList.map { persons =>
+        Ok(persons.foldLeft(JsArray(List()))( (obj, person) => obj ++ Json.arr(person) ))
+      }
+    }
+  }*/ 
+
 }
